717f3f5935da88ccbbcfdbf1861ae989571559e5
Juan Manuel Gallego <juanma.gallego@tiendanube.com>
2023-04-17 14:09:29 -0300

    feat: update components to nimbus2 and new design

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,15 +1,25 @@
 import { connect, iAmReady } from '@tiendanube/nexo/helpers';
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { ToastProvider } from '@nimbus-ds/components';
-
-import { ThemeProvider } from '@nimbus-ds/styles';
-import '@nimbus-ds/styles/dist/index.css';
-import '@nimbus-ds/styles/dist/themes/dark.css';
 
 import nexo from './nexoClient';
 import NexoSyncRoute from './NexoSyncRoute';
-import { ConfigurationPage, DeliveryPage, DetailPage, MainPage } from './pages';
+import {
+  ConfirmationModalExamplePage,
+  ExamplesPage,
+  FormExamplePage,
+  LoginExamplePage,
+  MainPage,
+  PageTemplateExamplePage,
+  ProductListExamplePage,
+  SettingsExamplePage,
+  SimpleListExamplePage,
+} from './pages';
+import { DarkModeProvider, ProductProvider, BaseLayout } from './components';
+import { generateProducts } from './lib';
+
+import { ToastProvider } from '@nimbus-ds/components';
+import ProductsPage from './pages/ProductsPage.tsx/ProductsPage';
 
 function App() {
   const [isConnect, setIsConnect] = useState(false);
@@ -23,29 +33,53 @@ function App() {
 
   if (!isConnect) return <div>connecting..</div>;
 
+  const initialProducts = generateProducts(30);
+
   return (
-    <BrowserRouter>
-      <ThemeProvider theme="base">
-        <ToastProvider>
-          <NexoSyncRoute>
-            <Switch>
-              <Route path="/" exact>
-                <MainPage />
-              </Route>
-              <Route path="/configuration">
-                <ConfigurationPage />
-              </Route>
-              <Route path="/delivery">
-                <DeliveryPage />
-              </Route>
-              <Route path="/detail">
-                <DetailPage />
-              </Route>
-            </Switch>
-          </NexoSyncRoute>
-        </ToastProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <DarkModeProvider>
+      <BrowserRouter>
+        <ProductProvider initialProducts={initialProducts}>
+          <ToastProvider>
+            <BaseLayout>
+              <NexoSyncRoute>
+                <Switch>
+                  <Route path="/" exact>
+                    <MainPage />
+                  </Route>
+                  <Route path="/examples">
+                    <ExamplesPage />
+                  </Route>
+                  <Route path="/products">
+                    <ProductsPage />
+                  </Route>
+                  <Route path="/examples-confirmation-modal">
+                    <ConfirmationModalExamplePage />
+                  </Route>
+                  <Route path="/examples-form">
+                    <FormExamplePage />
+                  </Route>
+                  <Route path="/examples-login">
+                    <LoginExamplePage />
+                  </Route>
+                  <Route path="/examples-page-template">
+                    <PageTemplateExamplePage />
+                  </Route>
+                  <Route path="/examples-product-list">
+                    <ProductListExamplePage />
+                  </Route>
+                  <Route path="/examples-settings">
+                    <SettingsExamplePage />
+                  </Route>
+                  <Route path="/examples-simple-list">
+                    <SimpleListExamplePage />
+                  </Route>
+                </Switch>
+              </NexoSyncRoute>
+            </BaseLayout>
+          </ToastProvider>
+        </ProductProvider>
+      </BrowserRouter>
+    </DarkModeProvider>
   );
 }
 
--- react/src/pages/MainPage/MainPage.tsx
+++ react/src/pages/MainPage/MainPage.tsx
@@ -1,43 +1,69 @@
-import { Icon, Button, Box, IconButton } from '@nimbus-ds/components';
-import { Page } from '@nimbus-ds/patterns';
-import { CogIcon } from '@nimbus-ds/icons';
-import { goTo, navigateHeaderRemove } from '@tiendanube/nexo/helpers';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import nexo from '../../nexoClient';
-import { useResponsive } from '../../stratus/hooks';
+import {
+  Icon,
+  Button,
+  Box,
+  IconButton,
+  Card,
+  Title,
+  Text,
+  Link,
+} from '@nimbus-ds/components';
+import { Layout, Page } from '@nimbus-ds/patterns';
+import {
+  ExternalLinkIcon,
+  PictureIcon,
+  PlusCircleIcon,
+  TagIcon,
+} from '@nimbus-ds/icons';
+import tokens from '@nimbus-ds/tokens/dist/js/tokens';
+import { goTo, navigateHeaderRemove } from '@tiendanube/nexo/helpers';
 
-import List from './components/List';
+import nexo from '../../nexoClient';
+import { useWindowWidth } from '../../hooks';
+import { useProductContext } from '../../components';
 
 function PageMain() {
   const { push } = useHistory();
-  const { isMobile } = useResponsive();
+  const windowWidth = useWindowWidth();
 
-  const handleGoToConfiguration = () => push('/configuration');
-  const handleGoToDelivery = () => push('/delivery');
+  const breakpointMd = tokens.breakpoint.md.value.replace('px', '');
+
+  const handleGoToExamples = () => push('/examples');
+  const handleGoToProducts = () => push('/products');
   const handleGoToOrders = () => goTo(nexo, '/orders');
 
-  const actions = isMobile ? (
-    <IconButton
-      backgroundColor="transparent"
-      borderColor="transparent"
-      onClick={handleGoToConfiguration}
-      source={<CogIcon />}
-      size="2rem"
-    />
-  ) : (
-    <Box display="flex" gap="2">
-      <Button onClick={handleGoToConfiguration}>
-        <Icon source={<CogIcon />} />
-        Configuración
-      </Button>
-      <Button onClick={handleGoToDelivery}>Envios personalizados</Button>
-      <Button appearance="primary" onClick={handleGoToOrders}>
-        Ir a ventas
-      </Button>
-    </Box>
-  );
+  const { products, addProducts } = useProductContext();
+  const productsQty = products.length;
+
+  const add5Products = () => addProducts(5);
+
+  const actions =
+    windowWidth !== null && windowWidth <= breakpointMd ? (
+      <IconButton
+        backgroundColor="transparent"
+        borderColor="transparent"
+        onClick={handleGoToExamples}
+        source={<PictureIcon />}
+        size="2rem"
+      />
+    ) : (
+      <Box display="flex" gap="2">
+        <Button onClick={handleGoToExamples}>
+          <Icon source={<PictureIcon />} color="currentColor" />
+          Ejemplos
+        </Button>
+        <Button onClick={handleGoToProducts}>
+          <Icon source={<TagIcon />} color="currentColor" />
+          Productos
+        </Button>
+        <Button appearance="primary" onClick={handleGoToOrders}>
+          Ir a ventas
+        </Button>
+      </Box>
+    );
 
   useEffect(() => {
     navigateHeaderRemove(nexo);
@@ -45,12 +71,74 @@ function PageMain() {
 
   return (
     <>
-      <Page maxWidth="1200px">
-        <Page.Header title="Pago Nube" buttonStack={actions} />
+      <Page maxWidth="800px">
+        <Page.Header title="Demo app" buttonStack={actions} />
         <Page.Body>
-          <Box display="flex" flexDirection="column" alignItems="stretch">
-            <List />
-          </Box>
+          <Layout columns="1">
+            <Layout.Section>
+              <Card>
+                <Card.Header title="¡Felicitaciones por crear tu app!" />
+                <Card.Body>
+                  <Text>
+                    ¡Hola! Tu app está casi lista. Cuenta con las bases sólidas
+                    que necesitás para comenzar, incluyendo nuestro&nbsp;
+                    <Link
+                      as="a"
+                      href="https://nimbus.tiendanube.com/"
+                      target="_blank"
+                      appearance="primary"
+                      textDecoration="none"
+                    >
+                      design system Nimbus
+                    </Link>
+                    &nbsp;y la integración a la&nbsp;
+                    <Link appearance="primary" textDecoration="none">
+                      API de Tiendanube
+                    </Link>
+                    .
+                    <br />
+                    <br />
+                    Con Nimbus a tu disposición, vas a poder crear interfaces de
+                    usuario intuitivas y atractivas con gran velocidad, mientras
+                    que la API de Tiendanube te permitirá aprovechar al máximo
+                    las funcionalidades de eCommerce. ¡Feliz desarrollo!
+                  </Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Link>
+                    Conocé más sobre cómo crear tu app
+                    <Icon color="currentColor" source={<ExternalLinkIcon />} />
+                  </Link>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Header title="Contador de productos de Tienda demo" />
+                <Card.Body>
+                  <Box display="flex" flexDirection="column" gap="4" mb="2">
+                    <Text>
+                      Los productos de ejemplo se crean con un nombre y precio
+                      aleatorios, a modo de prueba. Podés modificarlos o
+                      eliminarlos en cualquier momento.
+                    </Text>
+                    <Box display="flex" flexDirection="column" gap="2">
+                      <Text color="neutral-textDisabled">
+                        Total de productos
+                      </Text>
+                      <Title as="h6" fontSize="h1" color="primary-textLow">
+                        {productsQty}
+                      </Title>
+                    </Box>
+                  </Box>
+                </Card.Body>
+                <Card.Footer>
+                  <Button appearance="primary" onClick={add5Products}>
+                    <Icon color="currentColor" source={<PlusCircleIcon />} />
+                    Crear 5 productos
+                  </Button>
+                </Card.Footer>
+              </Card>
+            </Layout.Section>
+          </Layout>
         </Page.Body>
       </Page>
     </>
